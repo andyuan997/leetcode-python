a4415385ef84ae9ee24614092f7e0f7201717525
Andy Yuan <andyuan997@gmail.com>
2024-07-11 16:49:15 +0800

    Create 1. Two Sum.py

---
+++ 1. Two Sum.py
@@ -0,0 +1,31 @@
+class Solution(object):
+
+
+# 暴力破解
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i , j]
+
+
+# hash map
+
+    def twoSum(nums, target):
+        hash_map = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in hash_map:
+                return [hash_map[complement], i]
+            hash_map[num] = i
+        return []
+
+
+
+
+    print(twoSum([1,2,3,8], 11))
