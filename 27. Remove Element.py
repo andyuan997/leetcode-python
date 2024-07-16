ab18d54e70c994645eb4af7ef860dbc899ceb8da
Andy Yuan <andyuan997@gmail.com>
2024-07-17 02:26:11 +0800

    Create 27. Remove Element.py

---
+++ 27. Remove Element.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    
+    
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        k = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+        return k
+
+
+
+solution = Solution()
+nums = [1]
+val = 1
+print(solution.removeElement(nums, val))
