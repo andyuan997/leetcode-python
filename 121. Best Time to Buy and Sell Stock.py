641318b417b759c203b3d6be16ed847b43b31ee0
Andy Yuan <andyuan997@gmail.com>
2024-09-21 15:11:31 +0800

    Create 121. Best Time to Buy and Sell Stock.py

---
+++ 121. Best Time to Buy and Sell Stock.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxProfit(prices):
+        min_price = int
+        max_profit = 0
+
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            elif price - min_price > max_profit:
+                max_profit = price - min_price
+
+        return max_profit
